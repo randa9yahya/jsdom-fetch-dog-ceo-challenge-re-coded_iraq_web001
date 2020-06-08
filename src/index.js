e98732d5692c9026a50074e10f5acf5b339b77dc
randa yahya <randaalrayan96@gmial.com>
2020-06-08 08:57:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log('%c HI', 'color: firebrick')
+//varabels
+
+let dogImageContainter = document.getElementById('dog-image-container');
+let breedDropdown = document.getElementById('breed-dropdown');
+let dogBreeds = document.getElementById('dog-breeds');
